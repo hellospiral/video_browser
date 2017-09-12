1ef0e87746fc358becb109fe713f66e0eee8b029
Matt Carlson <matthewthomascarlson@gmail.com>
2017-09-12 14:05:07 -0700

    show video components in main app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDYj1qVw-KrxgWAP5SOH0mQf_JLXw6oS0I';
 
-// Create a new component to produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'Bicolor cat'}, (videos) => {
+      this.setState({ videos });
+    });
+
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
-// Take the component's html and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
